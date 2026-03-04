971d39f894451aa070da30bd1ce6a8c6ae823ba6
Aya33263 <ayaalzwghaibi@gmail.com>
2026-03-05 12:56:42 +1300

    Add files via upload

---
+++ ex5.last then first name.w3.py
@@ -0,0 +1,8 @@
+# Prompt the user to input their first name and store it in 'fname' variable
+fname = input("Input your First Name : ")
+
+# Prompt the user to input their last name and store it in 'lname' variable
+lname = input("Input your Last Name : ")
+
+# Display the greeting with the last name followed by the first name
+print("Hello  " + lname + " " + fname)
